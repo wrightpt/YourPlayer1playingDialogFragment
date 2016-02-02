363343964ee7bd8f3d50bc3d79ba0dd44912445b
c <c@c>
2016-02-02 13:28:04 -0500

    Initial commit

---
+++ app/src/main/java/com/yourplayer/c/yourplayer1/PlayerActivity.java
@@ -0,0 +1,41 @@
+package com.yourplayer.c.yourplayer1;
+
+import android.os.*;
+import android.widget.*;
+
+import com.google.android.youtube.player.*;
+
+/**
+ * Created by c on 1/30/16.
+ */
+public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private YouTubePlayerView playerView;
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        setContentView(R.layout.activity_player);
+
+        playerView = (YouTubePlayerView)findViewById(R.id.player_view);
+        playerView.initialize(YoutubeConnector.KEY, this);
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult result) {
+        Toast.makeText(this, getString(R.string.failed), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+                                        boolean restored) {
+        if(!restored){
+            player.cueVideo(getIntent().getStringExtra("VIDEO_ID"));
+        }
+    }
+
+
+
+}
